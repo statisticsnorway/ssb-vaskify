c64d4fd2ad1e2edf936b48f7447dab96bdd6808e
sjentoft <susiejentoft@gmail.com>
2024-11-29 09:24:37 +0000

    Initial commit

---
+++ src/vaskify/__main__.py
@@ -0,0 +1,13 @@
+"""Command-line interface."""
+
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """vaskify."""
+
+
+if __name__ == "__main__":
+    main(prog_name="ssb-vaskify")  # pragma: no cover
